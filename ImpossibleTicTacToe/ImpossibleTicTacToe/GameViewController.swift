3880d160fb4fd4ec89aaa32e5fdf9ddd5db2e931
Gustiin <ericgustin44@gmail.com>
2020-07-20 13:02:35 -0700

    Detect game over

--- ImpossibleTicTacToe/ImpossibleTicTacToe/GameViewController.swift
+++ ImpossibleTicTacToe/ImpossibleTicTacToe/GameViewController.swift
@@ -33,6 +33,8 @@ class GameViewController: UIViewController {
   
   private let xSystemName = "xmark"
   private let oSystemName = "circle"
+  private var isPlayer1Turn = true
+  private var turn = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -71,24 +73,24 @@ class GameViewController: UIViewController {
         boardClickableCompartments[row][col].addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(boardClicked)))
         boardClickableCompartments[row][col].translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(boardClickableCompartments[row][col])
-        boardClickableCompartments[row][col].widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width*19/60).isActive = true
-        boardClickableCompartments[row][col].heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width*19/60).isActive = true
+        boardClickableCompartments[row][col].widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width*18/60).isActive = true
+        boardClickableCompartments[row][col].heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width*18/60).isActive = true
         switch row {
           case 0:
-            boardClickableCompartments[row][col].centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -UIScreen.main.bounds.width*19/60).isActive = true
+            boardClickableCompartments[row][col].centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -UIScreen.main.bounds.width*32.5/100).isActive = true
           case 1:
             boardClickableCompartments[row][col].centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
           case 2:
-            boardClickableCompartments[row][col].centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: UIScreen.main.bounds.width*19/60).isActive = true
+            boardClickableCompartments[row][col].centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: UIScreen.main.bounds.width*32.5/100).isActive = true
           default: return
         }
         switch col {
           case 0:
-            boardClickableCompartments[row][col].centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: -UIScreen.main.bounds.width*19/60).isActive = true
+            boardClickableCompartments[row][col].centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: -UIScreen.main.bounds.width*1/3).isActive = true
           case 1:
             boardClickableCompartments[row][col].centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
           case 2:
-            boardClickableCompartments[row][col].centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: UIScreen.main.bounds.width*19/60).isActive = true
+            boardClickableCompartments[row][col].centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: UIScreen.main.bounds.width*1/3).isActive = true
           default: return
         }
       }
@@ -98,13 +100,32 @@ class GameViewController: UIViewController {
   }
   
   private func gameLoop() {
-    for turn in 1...9 {
+    if !isGameOver(currentPosition: board) {
+      turn += 1
       if turn % 2 == 1 {  // player1's turn
-        
+        isPlayer1Turn = true
       } else {  // computer's turn
-
+        isPlayer1Turn = false
+        //      var depth = 0
+        //      for row in 0...2 {
+        //        for col in 0...2 {
+        //          if board[row][col] == "" {
+        //            depth += 1
+        //          }
+        //        }
+        //      }
+        //      minimax(position: board, depth: depth, isMaximizingPlayer: true)
+        outer: for row in 0...2 {
+          for col in 0...2 {
+            if board[row][col] == "" {
+              makeSymbol(systemName: oSystemName, row: row, col: col)
+              print("break")
+              break outer
+            }
+          }
+        }
       }
-    }
+    } else { endGame() }
   }
   
   private func makeSymbol(systemName: String, row: Int, col: Int) {
@@ -131,13 +152,49 @@ class GameViewController: UIViewController {
         symbol.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: UIScreen.main.bounds.width*19/60).isActive = true
       default: return
     }
+    board[row][col] = systemName
+    gameLoop() // go to next turn
   }
   
   @objc func boardClicked(_ gestureRecognizer: UITapGestureRecognizer) {
+    if !isPlayer1Turn { return }
     guard gestureRecognizer.view != nil else { return }
     let row = Int(gestureRecognizer.view!.tag / 3)
     let col = gestureRecognizer.view!.tag % 3
-    makeSymbol(systemName: "xmark", row: row, col: col)
+    if board[row][col] == "" {
+      makeSymbol(systemName: xSystemName, row: row, col: col)
+    }
+  }
+  
+  private func minimax(position: [[String]], depth: Int, isMaximizingPlayer: Bool) {
+    //if depth == 0
+    gameLoop()
+//    makeSymbol(systemName: oSystemName, row: 2, col: 2)
+//    board[2][2] = "o"
+    
+  }
+  
+  private func isGameOver(currentPosition: [[String]]) -> Bool {
+    if (currentPosition[0][0] != "" && ((currentPosition[0][0] == currentPosition[0][1] && currentPosition[0][0] == currentPosition[0][2]) ||
+      (currentPosition[0][0] == currentPosition[1][0] && currentPosition[0][0] == currentPosition[2][0]) ||
+      (currentPosition[0][0] == currentPosition[1][1] && currentPosition[0][0] == currentPosition[2][2]))) ||
+      (currentPosition[2][2] != "" && ((currentPosition[2][2] == currentPosition[2][1] && currentPosition[2][2] == currentPosition[2][0]) ||
+      currentPosition[2][2] == currentPosition[1][2] && currentPosition[2][2] == currentPosition[0][2])) ||
+      (currentPosition[1][1] != "" && ((currentPosition[1][1] == currentPosition[1][0] && currentPosition[1][1] == currentPosition[1][2]) ||
+      (currentPosition[1][1] == currentPosition[0][1] && currentPosition[1][1] == currentPosition[2][1]) ||
+      (currentPosition[1][1] == currentPosition[2][0] && currentPosition[1][1] == currentPosition[0][2]))) {
+      print("game over")
+      return true
+    }
+    return false
+  }
+  
+  private func endGame() {
+    for row in 0...2 {
+      for col in 0...2 {
+        boardClickableCompartments[row][col].isUserInteractionEnabled = false
+      }
+    }
   }
 }
 
